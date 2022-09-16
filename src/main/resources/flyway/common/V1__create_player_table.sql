4e2a43284c83fcb0fc7138bdd322e6b7a9731494
dataenggu <dataenggu@gmail.com>
2022-09-16 16:03:20 +0900

    add flyway environment

---
+++ src/main/resources/flyway/common/V1__create_player_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE player
+(
+    id       INT     NOT NULL AUTO_INCREMENT COMMENT 'id',
+    name     VARCHAR(100) NOT NULL COMMENT '이름',
+    age      INT NULL COMMENT '나이',
+    position VARCHAR(10) NOT NULL COMMENT '포지션',
+    back_no  INT     NOT NULL COMMENT '등번호',
+    moto     VARCHAR(100) NULL COMMENT '좌우명(or 경기에 임하는 각오)',
+    cur_yn   BOOLEAN NOT NULL COMMENT '팀 소속 여부',
+    PRIMARY KEY (id)
+) COMMENT = 'WTF 선수 테이블'
+;
