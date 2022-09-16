ca5689d457a4dca63a83309e51b6d618cfe750cd
dataenggu <dataenggu@gmail.com>
2022-09-16 10:20:31 +0900

    init

---
+++ src/main/java/com/wtf/webapp/wtfbe/WtfBeApplication.java
@@ -0,0 +1,13 @@
+package com.wtf.webapp.wtfbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtfBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WtfBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wtf/webapp/wtfbe/WtfBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wtf.webapp.wtfbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtfBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
