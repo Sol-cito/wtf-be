06ce483eb51f97a880b524e3d0ddb731ee6aa4a1
dataenggu <dsolkim1030@gmail.com>
2022-09-19 22:49:02 +0900

    edit shell script

--- deploy.sh
+++ deploy.sh
@@ -1,42 +1,34 @@
-function check_profile_status() {
-  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" "http://${1}:${2}/getProfile")
-  echo "${RESPONSE_CODE}"
-}
+function find_idle_service_name() {
+  CUR_PROFILE=$(curl -s "http://${1}:${2}/getProfile")
 
-function find_current_service_name() {
-  RESPONSE_CODE=$(check_profile_status $ADDRESS $PORT $BLUE_PORT)
-
-  if [ "${RESPONSE_CODE}" == 200 ]; then
-    CURRENT_PROFILE=${1}
+  if [ "${CUR_PROFILE}" == "blue" ]; then
+    echo "green"
   else
-    CURRENT_PROFILE=$(curl -s "http://${2}:${3}/getProfile")
+    echo "blue"
   fi
-  echo "${CURRENT_PROFILE}"
 }
 
 function find_idle_port() {
-  if [ "${1}" == "${2}" ]; then
-    echo "${3}"
+  CUR_PROFILE=$(curl -s "http://${1}:${2}/getProfile")
+
+  if [ "${CUR_PROFILE}" == "blue" ]; then
+    echo ${4}
   else
-    echo "${4}"
+    echo ${3}
   fi
 }
 
 ADDRESS="localhost"
 
-PORT=8096
+PORT=8080
 
-BLUE_NAME="blue"
-GREEN_NAME="green"
 BLUE_PORT=8095
 GREEN_PORT=8096
 
-RESPONSE_CODE=$(check_profile_status $ADDRESS $PORT $BLUE_PORT)
+IDLE_PROFILE=$(find_idle_service_name $ADDRESS $PORT $BLUE_PORT $GREEN_PORT)
 
-CURRENT_PROFILE=$(find_current_service_name $BLUE_NAME $ADDRESS $PORT)
+IDLE_PORT=$(find_idle_port $ADDRESS $PORT $BLUE_PORT $GREEN_PORT)
 
-IDLE_PORT=$(find_idle_port "$CURRENT_PROFILE" $BLUE_NAME $BLUE_PORT $GREEN_PORT)
-
-echo "${RESPONSE_CODE}"
-echo "${CURRENT_PROFILE}"
+echo "${IDLE_PROFILE}"
 echo "${IDLE_PORT}"
+
