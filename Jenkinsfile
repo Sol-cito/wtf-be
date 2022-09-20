bbc6bd5f64ee620aabba92099d11e72f88c2c84f
dataenggu <dataenggu@gmail.com>
2022-09-20 09:56:28 +0900

    edit shell script and jenkinfile

--- Jenkinsfile
+++ Jenkinsfile
@@ -10,21 +10,9 @@ pipeline {
 
         stage('Build') {
             steps {
-                echo "Build start"
+                echo "Build start by shell script"
+                sh 'deploy.sh'
             }
         }
-//         stage('Build') {
-//             steps {
-//                 sh "cd /var/lib/jenkins/jobs/wtf-fe-dev/workspace/"
-//                 sh "npm install --save --legacy-peer-deps"
-//                 sh "CI= npm run build"
-//             }
-//         }
-//         stage('Move Build Folder to Project dir') {
-//             steps {
-//                 sh "sudo rm -rf /home/sol/project/wtf-fe/build"
-//                 sh "sudo cp -r /var/lib/jenkins/jobs/wtf-fe-dev/workspace/build /home/sol/project/wtf-fe/"
-//             }
-//         }
     }
 }
--- deploy.sh
+++ deploy.sh
@@ -12,12 +12,23 @@ function find_idle_port() {
   CUR_PROFILE=$(curl -s "http://${1}:${2}/profile")
 
   if [ "${CUR_PROFILE}" == "blue" ]; then
-    echo ${4}
+    echo "${4}"
   else
-    echo ${3}
+    echo "${3}"
   fi
 }
 
+function find_current_port() {
+  CUR_PROFILE=$(curl -s "http://${1}:${2}/profile")
+
+  if [ "${CUR_PROFILE}" == "blue" ]; then
+    echo "${3}"
+  else
+    echo "${4}"
+  fi
+}
+
+# Variables
 ADDRESS="localhost"
 
 PORT=8080
@@ -25,10 +36,71 @@ PORT=8080
 BLUE_PORT=8095
 GREEN_PORT=8096
 
+# check idle profile and port
 IDLE_PROFILE=$(find_idle_service_name $ADDRESS $PORT $BLUE_PORT $GREEN_PORT)
 
 IDLE_PORT=$(find_idle_port $ADDRESS $PORT $BLUE_PORT $GREEN_PORT)
 
-echo "${IDLE_PROFILE}"
-echo "${IDLE_PORT}"
+echo "> idle profile : ${IDLE_PROFILE}"
+echo "> idle port : ${IDLE_PORT}"
 
+# check pid and kill process
+echo "> check running app pid on ${IDLE_PORT}"
+IDLE_PID=$(lsof -ti tcp:"${IDLE_PORT}")
+
+echo "> ${IDLE_PID}"
+if [ -z "${IDLE_PID}" ]
+then
+  echo "> there is no currently running app on ${IDLE_PID}"
+else
+  echo "> kill -15 ${IDLE_PID}"
+  kill -15 "${IDLE_PID}"
+  sleep 5
+fi
+
+# create .jar
+JENKINS_WORKSPACE=/var/lib/jenkins/jobs/wtf-be-dev/workspace
+REPOSITORY=/home/sol/project/wtf-be-dev/$IDLE_PROFILE
+
+cd "${JENKINS_WORKSPACE}"
+./gradlew build
+cp "${JENKINS_WORKSPACE}"/libs/*.jar "${REPOSITORY}"
+
+
+# Deploy
+cd "${REPOSITORY}"
+echo "> Deploy new application jar"
+JAR_NAME=$(ls -tr "${REPOSITORY}"/*.jar | tail -n 1)
+
+echo "> JAR Name: ${JAR_NAME}"
+
+echo "> add execution permission to ${JAR_NAME}"
+
+chmod +x "${JAR_NAME}"
+
+echo "> execute ${JAR_NAME}"
+
+java -jar -Dspring.profiles.active="${IDLE_PORT}" "${JAR_NAME}"
+
+# switch NGINX
+echo "> Port switch"
+echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/wtf-service-url.inc
+
+echo "> Reload NGINX"
+sudo systemctl restart nginx
+
+# kill current process
+CURRENT_PORT=$(find_current_port $ADDRESS $PORT $BLUE_PORT $GREEN_PORT)
+echo "> current port : ${CURRENT_PORT}"
+
+CURRENT_PID=$(lsof -ti tcp:"${CURRENT_PORT}")
+echo "> ${CURRENT_PID}"
+
+if [ -z "${CURRENT_PID}" ]
+then
+  echo "> there is no currently running app on ${CURRENT_PID}"
+else
+  echo "> kill -15 ${CURRENT_PID}"
+  kill -15 "${CURRENT_PID}"
+  sleep 5
+fi
