43cd6fdd14351256317fcd7da45f44aff151e8d6
dataenggu <dataenggu@gmail.com>
2022-09-20 11:42:43 +0900

    delete SpringBootTest on test class

--- src/test/java/com/wtf/webapp/wtfbe/WtfBeApplicationTests.java
+++ src/test/java/com/wtf/webapp/wtfbe/WtfBeApplicationTests.java
@@ -3,7 +3,7 @@ package com.wtf.webapp.wtfbe;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest - contextLoad exception
 class WtfBeApplicationTests {
 
     @Test
