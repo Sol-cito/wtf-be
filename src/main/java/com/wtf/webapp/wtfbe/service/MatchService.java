f5314dcb2349e3b8de54263c2e493e51c7ff82db
dataenggu <dataenggu@gmail.com>
2022-10-19 19:29:29 +0900

    Apply stream().map

--- src/main/java/com/wtf/webapp/wtfbe/service/MatchService.java
+++ src/main/java/com/wtf/webapp/wtfbe/service/MatchService.java
@@ -1,12 +1,16 @@
 package com.wtf.webapp.wtfbe.service;
 
 import com.wtf.webapp.wtfbe.dto.MatchResultDto;
+import com.wtf.webapp.wtfbe.dto.PlayerDto;
+import com.wtf.webapp.wtfbe.entity.MatchResultEntity;
 import com.wtf.webapp.wtfbe.repository.MatchResultRepository;
 import com.wtf.webapp.wtfbe.vo.JPQLParamVO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -23,7 +27,8 @@ public class MatchService {
                 .limit(limit)
                 .order(order)
                 .build();
-        List<MatchResultDto> result = jpqlBuilderService.getJQPLResult(jpqlParamVO);
-        return result;
+        List<MatchResultEntity> matchResultEntityResult = jpqlBuilderService.getJQPLResult(jpqlParamVO);
+
+        return matchResultEntityResult.stream().map(entity -> entity.convertToDto()).collect(Collectors.toList());
     }
 }
