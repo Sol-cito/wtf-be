c78f34eae6b1124c67aa9706e9f0e35b26c426f1
dataenggu <dataenggu@gmail.com>
2022-11-03 17:19:46 +0900

    Add flyway

---
+++ src/main/resources/flyway/common/V25__update_team_table.sql
@@ -0,0 +1,19 @@
+UPDATE team
+SET team_logo_src = "/team/logo_yellow.png"
+WHERE id = 1;
+
+UPDATE team
+SET team_logo_src = "/team/ykfc_logo.png"
+WHERE id = 3;
+
+UPDATE team
+SET team_logo_src = "/team/fcpassione_logo.png"
+WHERE id = 4;
+
+UPDATE team
+SET team_logo_src = "/team/footplr_logo.png"
+WHERE id = 6;
+
+UPDATE team
+SET team_logo_src = "/team/iemu11_logo.png"
+WHERE id = 7;
