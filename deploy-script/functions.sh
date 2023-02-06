315f5f472ae6078181a02bedad2bfa1b8a615682
dataenggu <dsolkim1030@gmail.com>
2023-02-06 16:46:10 +0900

    rollback log on functions.sh

--- deploy-script/functions.sh
+++ deploy-script/functions.sh
@@ -11,8 +11,6 @@ function find_idle_service_name() {
 function find_idle_port() {
   CUR_PROFILE=$(curl -s -k "${1}://${2}:${3}/profile")
 
-  echo "> find_idle_port function result (current running profile) : ${CUR_PROFILE}"
-
   if [ "${CUR_PROFILE}" == "blue" ]; then
     echo "${5}"
   else
