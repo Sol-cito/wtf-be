a2bb6c7b4cc69a8ebc98032fc966e10b4e7dd13d
dataenggu <dsolkim1030@gmail.com>
2023-02-06 16:16:47 +0900

    apply SSL for prod build

--- deploy-script/functions.sh
+++ deploy-script/functions.sh
@@ -1,5 +1,5 @@
 function find_idle_service_name() {
-  CUR_PROFILE=$(curl -s "http://${1}:${2}/profile")
+  CUR_PROFILE=$(curl -s -k "${1}://${2}:${3}/profile")
 
   if [ "${CUR_PROFILE}" == "blue" ]; then
     echo "green"
@@ -9,11 +9,11 @@ function find_idle_service_name() {
 }
 
 function find_idle_port() {
-  CUR_PROFILE=$(curl -s "http://${1}:${2}/profile")
+  CUR_PROFILE=$(curl -s -k "${1}://${2}:${3}/profile")
 
   if [ "${CUR_PROFILE}" == "blue" ]; then
-    echo "${4}"
+    echo "${5}"
   else
-    echo "${3}"
+    echo "${4}"
   fi
 }
