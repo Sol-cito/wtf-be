b770961de020a8e62aef6386b94328d70f4e0714
sol <dataenggu@gmail.com>
2023-03-04 18:50:21 +0100

    Implemented team modification

--- src/main/java/com/wtf/webapp/wtfbe/controller/PutController.java
+++ src/main/java/com/wtf/webapp/wtfbe/controller/PutController.java
@@ -1,14 +1,15 @@
 package com.wtf.webapp.wtfbe.controller;
 
-import com.wtf.webapp.wtfbe.dto.MatchResultDto;
-import com.wtf.webapp.wtfbe.dto.MatchResultRequestDto;
-import com.wtf.webapp.wtfbe.dto.PlayerDto;
-import com.wtf.webapp.wtfbe.dto.PlayerMultipartDto;
+import com.wtf.webapp.wtfbe.dto.*;
 import com.wtf.webapp.wtfbe.service.MatchService;
 import com.wtf.webapp.wtfbe.service.PlayerService;
+import com.wtf.webapp.wtfbe.service.TeamService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.HttpStatus.OK;
 
@@ -17,6 +18,7 @@ import static org.springframework.http.HttpStatus.OK;
 public class PutController {
     private final PlayerService playerService;
     private final MatchService matchService;
+    private final TeamService teamService;
 
     @PutMapping(path = "/player")
     public ResponseEntity<PlayerDto> modifyPlayer(@ModelAttribute PlayerMultipartDto playerMultipartDto) throws Exception {
@@ -27,6 +29,12 @@ public class PutController {
     @PutMapping(path = "/match")
     public ResponseEntity<MatchResultDto> modifyPlayer(@RequestBody MatchResultRequestDto request) {
         MatchResultDto result = matchService.handleMatchResult(request);
+        return new ResponseEntity<>(result, OK);
+    }
+
+    @PutMapping(path = "/team")
+    public ResponseEntity<TeamDto> modifyTeam(@ModelAttribute TeamMultipartDto request) throws Exception {
+        TeamDto result = teamService.modifyTeam(request).convertToTeamDto();
         return new ResponseEntity<>(result, OK);
     }
 }
